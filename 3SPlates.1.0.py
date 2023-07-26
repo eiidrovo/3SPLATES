b67829b97ac39c0c27993d9e4c1434a9289d6e09
eiidrovo <eiidrovo@espol.edu.ec>
2023-07-25 19:43:29 -0500

    update 2, simplified version of the script

---
+++ 3SPlates.1.0.py
@@ -0,0 +1,247 @@
+import numpy as np
+import json
+
+# This code was made by ESAU IDROVO (ESPOL -Ecuador)
+
+
+def mngen (parity, terms):
+    numbers=[]
+    if parity == "even":
+        for i in range(0,2*terms,2):
+            numbers.append(i)
+    elif parity == "odd":
+        for i in range(1,2*terms,2):
+            numbers.append(i)
+        
+    elif parity == "none":
+        for i in range(1,terms+1):
+            numbers.append(i)
+    else:
+        return(None)
+    mn=np.array(np.meshgrid(numbers, numbers)).T.reshape(-1, 2)
+    #Return an array with 2 columns with a combination of all m and n terms
+    return(mn)
+
+def platecoords(lenghtx, lenghty, subdititions):
+    x =np.linspace(0, lenghtx, subdititions+1)
+    y =np.linspace(0, lenghty, subdititions+1)
+    xx, yy = np.meshgrid(x, y)
+    coordinates = np.column_stack((xx.ravel(), yy.ravel()))
+    return (coordinates)
+
+# LOAD DISTRIBUTION #
+def load_dist (p, mnterms, platecoords, lenghtx, lenghty):
+    pxy= np.zeros(  np.shape(platecoords)[0])
+    for xy in enumerate(platecoords):
+        for mn in mnterms:
+            Pmn = 16*p/(mn[0]*mn[1]*np.pi**2)
+            pxy[xy[0]]+= Pmn * np.sin(mn[0]*np.pi*xy[1][0]/lenghtx)* np.sin(mn[1]*np.pi*xy[1][1]/lenghty)
+    pxy=np.column_stack((platecoords,pxy))
+    #Return an array with 3 columns [x, y, p]
+    return(pxy)
+
+def load_point(F, mnterms, platecoords, lenghtx, lenghty,posx,posy):
+    pxy= np.zeros(np.shape(platecoords)[0])
+    for xy in enumerate(platecoords):
+        for mn in mnterms:
+            Pmn =(4*F/(lenghtx*lenghty))*(np.sin(mn[0]*np.pi*posx/lenghtx))*(np.sin(mn[1]*np.pi*posy/lenghty))
+            pxy[xy[0]]+= Pmn* np.sin(mn[0]*np.pi*xy[1][0]/lenghtx)* np.sin(mn[1]*np.pi*xy[1][1]/lenghty)
+    pxy=np.column_stack((platecoords,pxy))
+    return(pxy)
+
+#DEFLECTION
+
+def deflect_dist (p, mnterms, platecoords, lenghtx, lenghty):
+    wxy= np.zeros(np.shape(platecoords)[0])
+    for xy in enumerate(platecoords):
+        for mn in mnterms:    
+            Pmn=16*p/(mn[0]*mn[1]*np.pi**2)
+            Wmn = Pmn / (D * np.pi**4 * ((mn[0]/lenghtx)**2+(mn[1]/lenghty)**2)**2)
+            wxy[xy[0]] += Wmn *  np.sin(mn[0]*np.pi*xy[1][0]/lenghtx)* np.sin(mn[1]*np.pi*xy[1][1]/lenghty)
+    wxy=np.column_stack((platecoords,wxy))
+
+    #Return an array with 3 columns [x, y, w]
+    return(wxy)
+
+def deflect_point (F, mnterms, platecoords, lenghtx, lenghty,posx,posy):
+    wxy= np.zeros(np.shape(platecoords)[0])
+    for xy in enumerate(platecoords):
+        for mn in mnterms:
+            Pmn =(4*F/(lenghtx*lenghty))*(np.sin(mn[0]*np.pi*posx/lenghtx))*(np.sin(mn[1]*np.pi*posy/lenghty))
+            Wmn = Pmn / (D * np.pi**4 * ((mn[0]/lenghtx)**2+(mn[1]/lenghty)**2)**2)
+            wxy[xy[0]]+= Wmn* np.sin(mn[0]*np.pi*xy[1][0]/lenghtx)* np.sin(mn[1]*np.pi*xy[1][1]/lenghty)
+    wxy=np.column_stack((platecoords,wxy))
+    #Return an array with 3 columns [x, y, w]
+    return(wxy)
+
+#Moments
+#dist
+
+def momentx_dist(p, mnterms, platecoords, lenghtx, lenghty):
+    Mx= np.zeros(np.shape(platecoords)[0])
+    for xy in enumerate(platecoords):
+        for mn in mnterms:
+            Pmn=16*p/(mn[0]*mn[1]*np.pi**2)
+            Wmn = Pmn / (D * np.pi**4 * ((mn[0]/lenghtx)**2+(mn[1]/lenghty)**2)**2)
+            Mx[xy[0]]+= Wmn * ((mn[0]*np.pi**2/lenghtx)+pois*(mn[1]*np.pi/lenghty)**2) * np.sin(mn[0]*np.pi*xy[1][0]/lenghtx)* np.sin(mn[1]*np.pi*xy[1][1]/lenghty)
+    Mx = D * Mx
+    Mx=np.column_stack((platecoords,Mx))
+    #Return an array with 3 columns [x, y, Mx]
+    return(Mx)
+
+def momenty_dist(p, mnterms, platecoords, lenghtx, lenghty):
+    My= np.zeros(np.shape(platecoords)[0])
+    for xy in enumerate(platecoords):
+        for mn in mnterms:
+            Pmn=16*p/(mn[0]*mn[1]*np.pi**2)
+            Wmn = Pmn / (D * np.pi**4 * ((mn[0]/lenghtx)**2+(mn[1]/lenghty)**2)**2)
+            My[xy[0]]+= Wmn * ((mn[1]*np.pi**2/lenghtx)+pois*(mn[0]*np.pi/lenghty)**2) * np.sin(mn[0]*np.pi*xy[1][0]/lenghtx)* np.sin(mn[1]*np.pi*xy[1][1]/lenghty)
+    My = D * My
+    My=np.column_stack((platecoords,My))
+    #Returns an array with 3 columns [x, y, My]
+    return(My)
+
+#point
+def momentx_point(F, mnterms, platecoords, lenghtx, lenghty,posx,posy):
+    Mx= np.zeros(np.shape(platecoords)[0])
+    for xy in enumerate(platecoords):
+        for mn in mnterms:
+            Pmn=(4*F/(lenghtx*lenghty))*(np.sin(mn[0]*np.pi*posx/lenghtx))*(np.sin(mn[1]*np.pi*posy/lenghty))
+            Wmn = Pmn / (D * np.pi**4 * ((mn[0]/lenghtx)**2+(mn[1]/lenghty)**2)**2)
+            Mx[xy[0]]+= Wmn * ((mn[0]*np.pi**2/lenghtx)+pois*(mn[1]*np.pi/lenghty)**2) * np.sin(mn[0]*np.pi*xy[1][0]/lenghtx)* np.sin(mn[1]*np.pi*xy[1][1]/lenghty)
+    Mx = D * Mx
+    Mx=np.column_stack((platecoords,Mx))
+    #Returns an array with 3 columns [x, y, Mx]
+    return(Mx)
+
+def momenty_point(F, mnterms, platecoords, lenghtx, lenghty,posx,posy):
+    My= np.zeros(np.shape(platecoords)[0])
+    for xy in enumerate(platecoords):
+        for mn in mnterms:
+            Pmn=(4*F/(lenghtx*lenghty))*(np.sin(mn[0]*np.pi*posx/lenghtx))*(np.sin(mn[1]*np.pi*posy/lenghty))
+            Wmn = Pmn / (D * np.pi**4 * ((mn[0]/lenghtx)**2+(mn[1]/lenghty)**2)**2)
+            My[xy[0]]+= Wmn * ((mn[1]*np.pi**2/lenghtx)+pois*(mn[0]*np.pi/lenghty)**2) * np.sin(mn[0]*np.pi*xy[1][0]/lenghtx)* np.sin(mn[1]*np.pi*xy[1][1]/lenghty)
+    My = D * My
+    My=np.column_stack((platecoords,My))
+    #Returns an array with 3 columns [x, y, My]
+    return(My)
+
+#Normal stresses
+
+
+def normalstresx(factor, momentx):
+    momentx[:,2] *= factor
+    sigmax = momentx
+    #Return an array with 3 columns [x, y, sigmax]
+    return(sigmax)
+
+def normalstresy(factor, momenty):
+    momenty[:,2] *= factor
+    sigmay = momenty
+    #Return an array with 3 columns [x, y, sigmay]
+    return(sigmay)
+
+
+
+# IMPORT DATA
+data = json.load(open('plate.json'))
+E = data["E"]
+Yield = data["yield"]
+pois = data["pois"]
+lenghtx = data ["lenghtx"]
+lenghty = data ["lenghty"]
+thickness = data ["thickness"]
+z=data ["z"]
+F = data["F"]
+XF = data["XF"]
+YF = data["YF"]
+resolution = data["Resolution"]
+parity = data["parity"]
+terms = data["terms"]
+typeofforce= data["type"]
+#Flexural Rigidity
+D = E*thickness**3/(12*(1-pois**2))
+#Factor for normal stress
+factor= 12*z/thickness**3
+
+mn=mngen(parity, terms)
+
+
+coords=platecoords(lenghtx,lenghty,resolution)
+
+if typeofforce == "distributed":
+    load = load_dist(F, mn, coords, lenghtx,lenghty)
+    deflection= deflect_dist(F, mn, coords,lenghtx, lenghty)
+    momentx = momentx_dist(F, mn, coords, lenghtx, lenghty)
+    momenty = momenty_dist(F, mn, coords, lenghtx, lenghty)
+    sigmax = normalstresx(factor, momentx)
+    sigmay = normalstresx(factor, momenty)
+
+    maxdefl = np.max(np.abs(deflection[:,2]))
+    maxsigmax = np.max(np.abs(sigmax[:,2]))
+    maxsigmay = np.max(np.abs(sigmay[:,2]))
+
+
+    ## this array contains the results x, y, pxy, mx, my, sigmx, sigmy in this order.
+    outputarray = np.column_stack((coords,load[:,2], momentx[:,2],momenty[:,2],sigmax[:,2], sigmay[:,2]))
+
+    np.savetxt('output.log',
+            outputarray,
+            newline='\n',
+            delimiter=',',
+            header='''
+
+    Output.
+    EIIS (2023)- ESPOL
+    https://github.com/eiidrovo/3SPLATES    
+    Max deflection [m] = {}
+    Max stress in x [N/m2] = {}
+    Max stress in y [N/m2] = {}
+   
+    x[m]    y[m]    P[N/m2]     Mx[N]       My[N]       Sigma x[N/m2]        Sigma y [Nm/m2] \n
+    
+
+    '''.format(maxdefl, maxsigmax, maxsigmay))   
+
+
+elif typeofforce == "point":
+    load = load_point(F, mn, coords, lenghtx,lenghty, XF, YF)
+    deflection= deflect_point(F, mn, coords, lenghtx,lenghty, XF, YF)
+    momentx = momentx_point(F, mn, coords, lenghtx,lenghty, XF, YF)
+    momenty = momenty_point(F, mn, coords, lenghtx,lenghty, XF, YF)
+    sigmax = normalstresx(factor, momentx)
+    sigmay = normalstresx(factor, momenty)
+
+    # With this values you can check if the plate satisfies the allowable values or not making an if or something extra
+    maxdefl = np.max(np.abs(deflection[:,2]))
+    maxsigmax = np.max(np.abs(sigmax[:,2]))
+    maxsigmay = np.max(np.abs(sigmay[:,2]))
+
+    
+    ## this array contains the results x, y, pxy, mx, my, sigmx, sigmy
+    outputarray = np.column_stack((coords,load[:,2], momentx[:,2],momenty[:,2],sigmax[:,2], sigmay[:,2]))
+    # Output array has 7 columns: x, y,p, Mx, My, Sigmax, Sigmay
+
+    np.savetxt('output.log',
+            outputarray,
+            newline='\n',
+            delimiter=',',
+            header='''
+
+    Output.
+    EIIS (2023)- ESPOL
+    https://github.com/eiidrovo/3SPLATES    
+    Max deflection [m] = {}
+    Max stress in x [N/m2] = {}
+    Max stress in y [N/m2] = {}
+   
+    x[m]    y[m]    P[N/m2]     Mx[N]       My[N]       Sigma x[N/m2]        Sigma y [Nm/m2] \n
+    
+
+    '''.format(maxdefl, maxsigmax, maxsigmay))   
+
+
+else:
+    print("Cannot being calculated anything, review your spelling in the plate.json file")
+    exit()
+
--- readme.md
+++ readme.md
@@ -2,26 +2,27 @@
 
 This repo was made by [Esau Idrovo](mailto:eiidrovo@protonmail.com), student of Naval Engineering at ESPOL - Ecuador. This script is open to everyone, but if it is useful to you, give me an Star and reference this repo into your work.
 
-## Sinusoudal Simple Supported Plates
+## Sinusoidal Simple Supported Plates
 
-This repository is used for the calculation of:
+This repository can be used for the calculation of:
 
 - Load distribution
 - Deflection
 - Bending moments
 - Normal stresses
 
-In plates using sinusoidal load approximation.
+In plates using sinusoidal load approximation for simple supported rectangular plates with 2 kind of loads: **point** and **distributed** along the plate.
 
 ## How to use the script?
 
 First! Check out the [dependencies](#dependencies) and install them before start (**recommendation**: keep it simple, download python from its official website, don't use PyCharm, Spyder or whatever, you won't need it).
 
-Download the [plate.json]() and [3SPlates.1.0.py]() files.
+Download the [plate.json](https://github.com/eiidrovo/3SPLATES/blob/main/plate.json) and [3SPlates.1.0.py](https://github.com/eiidrovo/3SPLATES/blob/main/3SPlates.1.0.py) files.
 
-Edit the [plate.json]() file with the dimmensions of the plate detailed below:
+Edit the [plate.json](https://github.com/eiidrovo/3SPLATES/blob/main/plate.json) file with the dimmensions of the plate detailed below:
 
 - E: Young Modulus  $\left[\frac{N}{m^2}\right]$
+- Yield: The yield point or maximun allowable stress $\left[\frac{N}{m^2}\right]$
 - Pois: Poisson Ratio
 - Lenghtx and lenghty: Lenght in x and y of the plate $\left[m\right]$
 - Thickness: thickness of the plate $\left[m\right]$
@@ -31,25 +32,32 @@ Edit the [plate.json]() file with the dimmensions of the plate detailed below:
 - Type: Type of force ('point', 'distributed')
 - F: Force $\left[N\right]$ (point-type) or load acting on the plate $\left[\frac{N}{m^2}\right]$ (distributed-type)
 - XF and YF: If the force is point-type, are the x and y position of the force acting on the plate $\left[m\right]$ If the foce is distributed this value doesn't matter.
-- Strainx and Strainy: The position in x and y of the point where you wanna analyze the strain $\left[m\right]$ 
+
 - Parity: Parity of the m and n terms. It can take the values "odd", "even" and "none". Review the section "[Parity](#parity)"
 
 Open the terminal in the folder where you've downloaded the files. and run the script.
 
 <pre>
 python 3SPlates.1.0.py
+
+or
+
+py 3SPlates.1.0.py
 </pre>
 
-Then write **PLOT** if you want to make the calculations for all the plate: load distribution, deflection, bending moments, etc. It will generate a folder name CSV for each magnitude of analysis that you can plot with other software like LabPlot or EasyPlot (For the version 2.0 of the script, the plotting will be automated).
+Be sure that the python file is in the same folder as the plate.json file.
 
-But, if you wanna make an alaysis of strains in a singlepoint, write **STRAIN**. It will generate a file named **strains.log**. that contains the strains in x and y in the point you've specified.
+
+If everything worked, you will see a file named **output.log** with the maximun values of normal stresses and deflection, and also a csv-like columns with the values of the coordinates and bending moment, load distribution, etc. You can use that data to plot using other programs or python. 
+
+For now I will be providing only the output array with all the values necessary to plot, for the next version I will release the program that make plots also.
 
 
 ## <a  id="parity"></a>Parity
 
 For **Distributed-Load** the parity value must be **odd**.
 
-For **Point-Load** the parity value can be odd, eve or none, it depends on how many force your case need, for example: for a single point load (delta dirac) the **none** works.
+For **Point-Load** the parity value can be odd, eve or none, it depends on how many force your case need, for example: for a single point load (delta dirac) the **none** value works fine.
 
 
 ## <a  id="dependencies"></a>Dependencies
